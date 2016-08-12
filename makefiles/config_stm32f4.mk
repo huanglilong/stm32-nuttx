ba86f1b5bd1e672dd48425bd4f734756c288c794
huanglilong <huanglilongwk@outlook.com>
2016-08-12 17:49:06 +0800

    add hello app

--- makefiles/config_stm32f4.mk
+++ makefiles/config_stm32f4.mk
@@ -10,3 +10,5 @@ MODULES += platform
 
 # user applications
 MODULES += buttons
+
+MODULES += hello
--- src/buttons/builtin_commands.h
+++ src/buttons/builtin_commands.h
@@ -3,9 +3,11 @@
 #include <nuttx/binfmt/builtin.h>
 
 extern int buttons_main(int argc, char *argv[]);
+extern int hello_main(int argc, char *argv[]);
 
 const struct builtin_s g_builtins[] = {
     {"buttons_main", SCHED_PRIORITY_DEFAULT, 2048, buttons_main},
+	{"hello_main", SCHED_PRIORITY_DEFAULT, 2048, hello_main},
     {NULL, 0, 0, NULL}
 };
-const int g_builtin_count = 1;
+const int g_builtin_count = 2;
